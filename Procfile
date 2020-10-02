a6dde9f15ee655c554abd9f67e983e83e76c60a9
fastestmk <fastestmk330@gmail.com>
2020-10-02 22:09:50 +0530

    added clock to procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn file_uploader.wsgi
+clock: python scripts/delete_files.py
--- file_uploader/settings.py
+++ file_uploader/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-BASE_URL = "http://127.0.0.1:8000/uploader/"
+# BASE_URL = "http://127.0.0.1:8000/uploader/"
+BASE_URL = "https://file-share-2323413.herokuapp.com/uploader/"
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
